0760da4a2e3491d6124619d7529cc4d13391c457
Salvador Montiel <salvadormontiel8@gmail.com>
2020-02-08 23:34:55 -0600

    :tada: Initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,45 @@
+const mix = require('laravel-mix');
+require('laravel-mix-purgecss');
+const tailwindcss = require('tailwindcss');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+const postcssPurgecss = require('@fullhuman/postcss-purgecss') ({
+
+    // Specify the paths to all of the template files in your project
+    content: [
+        './*.html'
+    ],
+
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+mix.sass('src/app.scss', 'public/css')
+    //.scripts('src/js/app.js', 'public/js/app.js')
+    .copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/fonts')
+    .copyDirectory('src/images', 'public/images')
+    .copy('index.html', 'public')
+    .options({
+        processCssUrls: false,
+        postCss: [
+            tailwindcss('./tailwind.config.js'),
+            require('autoprefixer'),
+            ...process.env.NODE_ENV === 'production' ? [ postcssPurgecss ] : []
+        ],
+    })
+    .purgeCss({
+        enabled: process.env.NODE_ENV === 'production',
+        extensions: ['html'],
+        folders: ['./']
+    })
+    .setPublicPath('public');
