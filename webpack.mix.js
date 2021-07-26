7a94ba9161d8be9dac4e5adc3e3aaf7bd2077452
Salvador Montiel <salvadormontiel8@gmail.com>
2021-07-26 12:35:07 -0500

    :zap: Add SVG icons from materialdesignicons.com replacing FontAwesome icons

--- webpack.mix.js
+++ webpack.mix.js
@@ -26,7 +26,6 @@ const postcssPurgecss = require('@fullhuman/postcss-purgecss') ({
 
 mix.sass('src/app.scss', 'public/css')
     //.scripts('src/js/app.js', 'public/js/app.js')
-    .copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/fonts')
     .copyDirectory('src/favicon', 'public')
     .options({
         processCssUrls: false,
